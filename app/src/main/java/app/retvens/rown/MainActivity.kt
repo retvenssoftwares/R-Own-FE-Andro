5bb0ece0778e4ebe63cffdd383de7553bab6e13d
Arjun Gupta <arjungupta0817@gmail.com>
2023-03-27 22:23:45 +0530

    Designed Splash Screen

--- app/src/main/java/app/retvens/rown/MainActivity.kt
+++ app/src/main/java/app/retvens/rown/MainActivity.kt
@@ -1,11 +1,20 @@
 package app.retvens.rown
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import app.retvens.rown.authentication.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+        },2000)
     }
 }
