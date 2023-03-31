27ab0fdf76ab8b1c24b4f90476468952cbf649e8
Arjun Gupta <arjungupta0817@gmail.com>
2023-03-31 21:22:29 +0530

    White Status bar

--- app/src/main/java/app/retvens/rown/authentication/PersonalInformationPhone.kt
+++ app/src/main/java/app/retvens/rown/authentication/PersonalInformationPhone.kt
@@ -42,30 +42,10 @@ class PersonalInformationPhone : AppCompatActivity() {
 //        binding.profilePhone.setImageURI(imageUri)
     }
     lateinit var dialog: Dialog
-    private fun setWindowFlag(bits: Int, on: Boolean) {
-        val win = window
-        val winParams = win.attributes
-        if (on) {
-            winParams.flags = winParams.flags or bits
-        } else {
-            winParams.flags = winParams.flags and bits.inv()
-        }
-        win.attributes = winParams
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPersonalInformationPhoneBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-            window.statusBarColor = Color.TRANSPARENT
-        }
 
         cameraImageUri = createImageUri()!!
 
