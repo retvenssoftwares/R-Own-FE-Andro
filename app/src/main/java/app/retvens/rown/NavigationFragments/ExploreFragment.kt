0e3bd7f4cad5340ab7f326e3fa5644f2963e37a2
Arjun Gupta <arjungupta0817@gmail.com>
2023-04-13 01:45:20 +0530

    BottomSheet Dialoge

--- app/src/main/java/app/retvens/rown/NavigationFragments/ExploreFragment.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/ExploreFragment.kt
@@ -5,17 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
 import app.retvens.rown.R
+import app.retvens.rown.bottomsheet.BottomSheet
 
 class ExploreFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,5 +20,5 @@ class ExploreFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_explore, container, false)
     }
 
- 
+
 }
