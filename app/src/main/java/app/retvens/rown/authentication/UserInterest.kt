176a3ea497cdd505a7ecfc4ec63e417d945b21bb
Arjun Gupta <arjungupta0817@gmail.com>
2023-04-13 01:05:41 +0530

    user interest

--- app/src/main/java/app/retvens/rown/authentication/UserInterest.kt
+++ app/src/main/java/app/retvens/rown/authentication/UserInterest.kt
@@ -20,7 +20,10 @@ class UserInterest : AppCompatActivity() {
         binding.userName.text = "Hello, $username!"
 
         binding.cardContinueInterest.setOnClickListener {
-            startActivity(Intent(this,UserContacts::class.java))
+            val intent = Intent(applicationContext, UserContacts::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            intent.putExtra("name",username)
+            startActivity(intent)
         }
     }
 }
