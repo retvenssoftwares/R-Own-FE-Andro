ece1d9ce5941c5b13dd45bbc5874ba8b9c4f76a7
Arjun Gupta <arjungupta0817@gmail.com>
2023-04-14 16:39:51 +0530

    Home Enhanced

--- app/src/main/java/app/retvens/rown/NavigationFragments/HomeFragment.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/HomeFragment.kt
@@ -10,7 +10,7 @@ import androidx.fragment.app.FragmentActivity
 import app.retvens.rown.R
 import app.retvens.rown.bottomsheet.BottomSheet
 
-class HomeFragment : Fragment(), GestureDetector.OnGestureListener {
+class HomeFragment : Fragment() {
     lateinit var gestureDetector: GestureDetector
     private val swipeThreshold = 100
     private val swipeVelocityThreshold = 100
@@ -27,11 +27,7 @@ class HomeFragment : Fragment(), GestureDetector.OnGestureListener {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
 
-//        view?.setOnTouchListener(object : View.OnTouchListener {
-//            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-//
-//            }
-//        })
+
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,51 +35,4 @@ class HomeFragment : Fragment(), GestureDetector.OnGestureListener {
         val fragManager = (activity as FragmentActivity).supportFragmentManager
         fragManager?.let{bottomSheet.show(it, BottomSheet.TAG)}
     }
-//    fun onTouchEvent(event: MotionEvent): Boolean {
-//        return if (gestureDetector.onTouchEvent(event)) {
-//            true
-//        }
-//        else {
-////            super.onTouchEvent(event)
-//            super.requireActivity().onTouchEvent(event)
-////            view?.onTouchEvent(event)
-//        }
-//    }
-
-    override fun onShowPress(e: MotionEvent) {
-        return
-    }
-    override fun onDown(e: MotionEvent): Boolean {
-        return false
-    }
-    override fun onSingleTapUp(e: MotionEvent): Boolean {
-        return false
-    }
-    override fun onScroll(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
-        return false
-    }
-    override fun onLongPress(p0: MotionEvent) {
-        return
-    }
-    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-        try {
-            val diffY = e2.y - e1.y
-            val diffX = e2.x - e1.x
-            if (Math.abs(diffX) > Math.abs(diffY)) {
-                if (Math.abs(diffX) > swipeThreshold && Math.abs(velocityX) > swipeVelocityThreshold) {
-                    if (diffX > 0) {
-                        Toast.makeText(context, "Left to Right swipe gesture", Toast.LENGTH_SHORT).show()
-                    }
-                    else {
-                        Toast.makeText(context, "Right to Left swipe gesture", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-        catch (exception: Exception) {
-            exception.printStackTrace()
-        }
-        return true
-    }
-
 }
