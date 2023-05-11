b8592d73325936b29ba584b29e433918122b7c0e
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-11 14:04:02 +0530

    NotificationActivity

--- app/src/main/java/app/retvens/rown/Dashboard/notificationScreen/NotificationActivity.kt
+++ app/src/main/java/app/retvens/rown/Dashboard/notificationScreen/NotificationActivity.kt
@@ -2,6 +2,7 @@ package app.retvens.rown.Dashboard.notificationScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.content.ContextCompat
 import app.retvens.rown.R
 import app.retvens.rown.databinding.ActivityNotificationBinding
@@ -22,24 +23,40 @@ class NotificationActivity : AppCompatActivity() {
             binding.communityNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.connectionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.suggetionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
+
+            binding.layoutPersonal.visibility = View.VISIBLE
+            binding.layoutConnection.visibility = View.GONE
+            binding.layoutSuggetion.visibility = View.GONE
         }
         binding.communityNotification.setOnClickListener {
             binding.personalNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.communityNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.green_own))
             binding.connectionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.suggetionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
+
+            binding.layoutPersonal.visibility = View.VISIBLE
+            binding.layoutConnection.visibility = View.GONE
+            binding.layoutSuggetion.visibility = View.GONE
         }
         binding.connectionNotification.setOnClickListener {
             binding.personalNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.communityNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.connectionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.green_own))
             binding.suggetionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
+
+            binding.layoutPersonal.visibility = View.GONE
+            binding.layoutConnection.visibility = View.VISIBLE
+            binding.layoutSuggetion.visibility = View.GONE
         }
         binding.suggetionNotification.setOnClickListener {
             binding.personalNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.communityNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.connectionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
             binding.suggetionNotification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.green_own))
+
+            binding.layoutPersonal.visibility = View.GONE
+            binding.layoutConnection.visibility = View.GONE
+            binding.layoutSuggetion.visibility = View.VISIBLE
         }
 
     }
