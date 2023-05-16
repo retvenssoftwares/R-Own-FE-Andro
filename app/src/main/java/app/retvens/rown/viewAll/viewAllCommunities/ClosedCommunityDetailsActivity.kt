71e7a750f91e6ed238a09c87f0f5455e48b98185
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-16 16:07:51 +0530

    ClosedCommunityDetailsActivity

---
+++ app/src/main/java/app/retvens/rown/viewAll/viewAllCommunities/ClosedCommunityDetailsActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.rown.viewAll.viewAllCommunities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class ClosedCommunityDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_closed_community_details)
+    }
+}
