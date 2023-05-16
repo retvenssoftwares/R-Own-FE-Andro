5517130dfd3aacf3214706e452eb3d53b1a16f6b
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-16 16:04:52 +0530

    Post Details

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/home/PostDetailsActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.rown.NavigationFragments.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class PostDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_details)
+    }
+}
