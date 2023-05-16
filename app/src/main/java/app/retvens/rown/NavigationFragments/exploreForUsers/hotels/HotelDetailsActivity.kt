5065f5a42cf3801001c88bc0dd3581a837c18138
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-16 16:10:07 +0530

    HotelDetailsActivity

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/exploreForUsers/hotels/HotelDetailsActivity.kt
@@ -0,0 +1,22 @@
+package app.retvens.rown.NavigationFragments.exploreForUsers.hotels
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+import app.retvens.rown.databinding.ActivityHotelDetailsBinding
+
+class HotelDetailsActivity : AppCompatActivity() {
+    lateinit var binding : ActivityHotelDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHotelDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.openReview.setOnClickListener {
+            startActivity(Intent(applicationContext, HotelReviewsActivity::class.java))
+        }
+
+    }
+}
