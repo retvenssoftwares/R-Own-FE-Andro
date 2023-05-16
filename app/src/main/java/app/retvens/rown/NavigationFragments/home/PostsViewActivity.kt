10fc9b10499dd4b5fae1b4807373b875ce2e3639
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-16 16:07:32 +0530

    PostsViewActivity

--- app/src/main/java/app/retvens/rown/NavigationFragments/home/PostsViewActivity.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/home/PostsViewActivity.kt
@@ -1,12 +1,12 @@
-package app.retvens.rown.vendorsDetails
+package app.retvens.rown.NavigationFragments.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.retvens.rown.R
 
-class ViewAllVendorsActivity : AppCompatActivity() {
+class PostsViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_all_vendors)
+        setContentView(R.layout.activity_posts_view)
     }
 }
