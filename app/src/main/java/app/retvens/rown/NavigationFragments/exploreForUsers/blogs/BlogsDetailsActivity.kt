858632009137412a0bf30f24aa474ba27dd729dd
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-16 16:14:12 +0530

    Bottom

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/exploreForUsers/blogs/BlogsDetailsActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.rown.NavigationFragments.exploreForUsers.blogs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class BlogsDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blogs_details)
+    }
+}
