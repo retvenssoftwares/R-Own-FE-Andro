76174b2a0a0c31eeb9e0d80f64c628dc656c9c29
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-16 16:09:37 +0530

    EventDetailsActivity

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/exploreForUsers/events/EventDetailsActivity.kt
@@ -0,0 +1,13 @@
+package app.retvens.rown.NavigationFragments.exploreForUsers.events
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class EventDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+}
