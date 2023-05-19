42b2741c686168cf8c63040bda30d828bc0ff0c3
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-19 17:36:08 +0530

    Events items

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/eventsForHoteliers/AllEventsPostedActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.rown.NavigationFragments.eventsForHoteliers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class AllEventsPostedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_events_posted)
+    }
+}
