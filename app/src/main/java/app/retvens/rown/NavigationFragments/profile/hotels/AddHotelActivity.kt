7e34117698a4324dfd3a285d5412b50cdca031bf
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-19 05:56:50 +0530

    profile

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/profile/hotels/AddHotelActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.rown.NavigationFragments.profile.hotels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class AddHotelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_hotel)
+    }
+}
