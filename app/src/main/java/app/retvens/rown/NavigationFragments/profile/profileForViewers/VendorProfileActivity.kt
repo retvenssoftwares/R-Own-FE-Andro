de6fe9efc4554c9fe775d53692afbb85cceb5dca
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-19 06:01:56 +0530

    Profile

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/profile/profileForViewers/VendorProfileActivity.kt
@@ -0,0 +1,15 @@
+package app.retvens.rown.NavigationFragments.profile.profileForViewers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class VendorProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vendor_profile)
+
+
+
+    }
+}
