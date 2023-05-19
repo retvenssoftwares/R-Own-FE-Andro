7024521a740f9fe38f315fcb616d95a805c954b7
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-19 06:03:51 +0530

    Profile

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/profile/profileForViewers/UserProfileActivity.kt
@@ -0,0 +1,130 @@
+package app.retvens.rown.NavigationFragments.profile.profileForViewers
+
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
+import app.retvens.rown.NavigationFragments.profile.media.MediaFragment
+import app.retvens.rown.NavigationFragments.profile.polls.PollsFragment
+import app.retvens.rown.NavigationFragments.profile.settingForViewers.AboutProfileActivity
+import app.retvens.rown.NavigationFragments.profile.settingForViewers.ReportProfileActivity
+import app.retvens.rown.NavigationFragments.profile.settingForViewers.ShareQRActivity
+import app.retvens.rown.NavigationFragments.profile.status.StatusFragment
+import app.retvens.rown.R
+import app.retvens.rown.bottomsheet.BottomSheetProfileSetting
+import com.google.android.material.imageview.ShapeableImageView
+
+class UserProfileActivity : AppCompatActivity() {
+
+    private lateinit var setting : ImageView
+    lateinit var profile : ShapeableImageView
+    lateinit var name : TextView
+
+    lateinit var polls : TextView
+    lateinit var media : TextView
+    lateinit var status : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+
+        profile = findViewById(R.id.profile)
+        name = findViewById(R.id.profile_name)
+
+        polls = findViewById(R.id.polls)
+        media = findViewById(R.id.media)
+        status = findViewById(R.id.status)
+
+
+        polls.setOnClickListener {
+            polls.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            media.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_5))
+            status.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_5))
+
+//            val childFragment: Fragment = PollsFragment()
+//            val transaction: FragmentTransaction = childFragmentManager.beginTransaction()
+//            transaction.replace(R.id.child_profile_fragments_container, childFragment).commit()
+        }
+        media.setOnClickListener {
+            media.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            polls.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_5))
+            status.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_5))
+
+//            val childFragment: Fragment = MediaFragment()
+//            val transaction: FragmentTransaction = childFragmentManager.beginTransaction()
+//            transaction.replace(R.id.child_profile_fragments_container, childFragment).commit()
+        }
+        status.setOnClickListener {
+            media.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_5))
+            polls.setBackgroundColor(ContextCompat.getColor(this, R.color.grey_5))
+            status.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+
+//            val childFragment: Fragment = StatusFragment()
+//            val transaction: FragmentTransaction = childFragmentManager.beginTransaction()
+//            transaction.replace(R.id.child_profile_fragments_container, childFragment).commit()
+        }
+
+        setting = findViewById(R.id.profile_setting)
+        setting.setOnClickListener {
+
+            val dialogLanguage = Dialog(this)
+            dialogLanguage.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialogLanguage.setContentView(R.layout.bottom_profile_setting_for_viewers)
+            dialogLanguage.setCancelable(true)
+
+            dialogLanguage.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            dialogLanguage.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialogLanguage.window?.attributes?.windowAnimations = R.style.DailogAnimation
+            dialogLanguage.window?.setGravity(Gravity.BOTTOM)
+            dialogLanguage.show()
+
+            dialogLanguage.findViewById<LinearLayout>(R.id.about).setOnClickListener {
+                startActivity(Intent(this, AboutProfileActivity::class.java))
+                dialogLanguage.dismiss()
+            }
+
+            dialogLanguage.findViewById<LinearLayout>(R.id.report).setOnClickListener {
+                startActivity(Intent(this, ReportProfileActivity::class.java))
+                dialogLanguage.dismiss()
+            }
+
+            dialogLanguage.findViewById<LinearLayout>(R.id.shareQr).setOnClickListener {
+                startActivity(Intent(this, ShareQRActivity::class.java))
+                dialogLanguage.dismiss()
+            }
+
+            dialogLanguage.findViewById<LinearLayout>(R.id.shareProfileInMessage).setOnClickListener {
+
+                dialogLanguage.dismiss()
+            }
+
+            dialogLanguage.findViewById<LinearLayout>(R.id.shareProfileVia).setOnClickListener {
+
+                dialogLanguage.dismiss()
+            }
+
+            dialogLanguage.findViewById<LinearLayout>(R.id.copyUrl).setOnClickListener {
+
+                dialogLanguage.dismiss()
+            }
+
+//            val bottomSheet = BottomSheetProfileSetting()
+//            val fragManager = supportFragmentManager
+//            fragManager.let{bottomSheet.show(it, BottomSheetProfileSetting.WTP_TAG)}
+//            bottomSheet.setOnBottomSheetProfileSettingClickListener(this)
+        }
+
+    }
+}
---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/profile/setting/profileSetting/SyncContactsActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.rown.NavigationFragments.profile.setting.profileSetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class SyncContactsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sync_contacts)
+    }
+}
