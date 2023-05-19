aa62873206e735d4ec34b13e9ac5a5607de83f51
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-19 17:37:40 +0530

    Events

--- app/src/main/java/app/retvens/rown/NavigationFragments/EventFragment.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/EventFragment.kt
@@ -5,16 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import app.retvens.rown.NavigationFragments.eventsForHoteliers.EventCategoriesAdapter
 import app.retvens.rown.R
 
 
 class EventFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var categoryRecyclerView: RecyclerView
+    lateinit var eventCategoriesAdapter: EventCategoriesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +23,16 @@ class EventFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_event, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val welcome = view.findViewById<TextView>(R.id.welcome_name)
+        val sharedPreferencesName = context?.getSharedPreferences("SaveFullName", AppCompatActivity.MODE_PRIVATE)
+        val profileName = sharedPreferencesName?.getString("full_name", "").toString()
+        welcome.text = "Welcome, $profileName!"
+
+        categoryRecyclerView = view.findViewById(R.id.categoryRecyclerView)
+
     }
 }
--- app/src/main/java/app/retvens/rown/NavigationFragments/EventFragmentForHoteliers.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/EventFragmentForHoteliers.kt
@@ -1,24 +1,39 @@
 package app.retvens.rown.NavigationFragments
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import app.retvens.rown.NavigationFragments.eventsForHoteliers.AddEventActivity
+import app.retvens.rown.NavigationFragments.eventsForHoteliers.AllEventsPostedActivity
 import app.retvens.rown.NavigationFragments.eventsForHoteliers.CreateEventActivity
+import app.retvens.rown.NavigationFragments.eventsForHoteliers.EventCategoriesAdapter
+import app.retvens.rown.NavigationFragments.eventsForHoteliers.EventCategoriesData
 import app.retvens.rown.R
 import app.retvens.rown.bottomsheet.BottomSheetCountryStateCity
+import app.retvens.rown.viewAll.viewAllBlogs.ViewAllCategoriesAdapter
+import app.retvens.rown.viewAll.viewAllBlogs.ViewAllCategoriesData
 import com.google.android.material.textfield.TextInputEditText
 
-class EventFragmentForHoteliers : Fragment(), BottomSheetCountryStateCity.OnBottomCountryStateCityClickListener {
+class EventFragmentForHoteliers : Fragment() {
 
-    lateinit var et_location_event : TextInputEditText
-    lateinit var et_event_venue : TextInputEditText
-    lateinit var next : CardView
+    lateinit var categoryRecyclerView: RecyclerView
+    lateinit var eventCategoriesAdapter: EventCategoriesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,29 +46,50 @@ class EventFragmentForHoteliers : Fragment(), BottomSheetCountryStateCity.OnBott
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        et_location_event = view.findViewById(R.id.et_location_event)
-        et_event_venue = view.findViewById(R.id.et_event_venue)
-        next = view.findViewById(R.id.next_Event)
-        postUpdateEvent()
+        val welcome = view.findViewById<TextView>(R.id.welcome_name)
+        val sharedPreferencesName = context?.getSharedPreferences("SaveFullName", AppCompatActivity.MODE_PRIVATE)
+        val profileName = sharedPreferencesName?.getString("full_name", "").toString()
+        welcome.text = "Welcome, $profileName!"
+
+        categoryRecyclerView = view.findViewById(R.id.categoryRecyclerView)
+
+        view.findViewById<CardView>(R.id.addEvent).setOnClickListener {
+
+            val dialogLanguage = Dialog(requireContext())
+            dialogLanguage.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialogLanguage.setContentView(R.layout.bottom_sheet_add_event)
+            dialogLanguage.setCancelable(true)
+
+            dialogLanguage.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            dialogLanguage.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialogLanguage.window?.attributes?.windowAnimations = R.style.DailogAnimation
+            dialogLanguage.window?.setGravity(Gravity.BOTTOM)
+            dialogLanguage.show()
+
+            dialogLanguage.findViewById<LinearLayout>(R.id.add).setOnClickListener {
+                startActivity(Intent(context, AddEventActivity::class.java))
+                dialogLanguage.dismiss()
+            }
+            dialogLanguage.findViewById<LinearLayout>(R.id.myEvent).setOnClickListener {
+                startActivity(Intent(context, AllEventsPostedActivity::class.java))
+                dialogLanguage.dismiss()
+            }
+        }
+        getCategories()
     }
-    private fun postUpdateEvent() {
-        et_location_event.setOnClickListener {
-            val bottomSheet = BottomSheetCountryStateCity()
-            val fragManager = (activity as FragmentActivity).supportFragmentManager
-            fragManager.let{bottomSheet.show(it, BottomSheetCountryStateCity.CountryStateCity_TAG)}
-            bottomSheet.setOnCountryStateCityClickListener(this)
-        }
+    private fun getCategories() {
+        categoryRecyclerView.layoutManager = LinearLayoutManager(context)
+        categoryRecyclerView.setHasFixedSize(true)
 
-        et_event_venue.setOnClickListener {
-            Toast.makeText(context, "Will implement along with api", Toast.LENGTH_SHORT).show()
-        }
+        val blogs = listOf<EventCategoriesData>(
+            EventCategoriesData("Title 1"),
+            EventCategoriesData("Title 2"),
+            EventCategoriesData("Title 3"),
+            EventCategoriesData("Title 23"),
+        )
 
-        next.setOnClickListener {
-            startActivity(Intent(context, CreateEventActivity::class.java))
-        }
-    }
-
-    override fun bottomCountryStateCityClick(CountryStateCityFrBo: String) {
-        et_location_event.setText(CountryStateCityFrBo)
+        eventCategoriesAdapter = EventCategoriesAdapter(blogs, requireContext())
+        categoryRecyclerView.adapter = eventCategoriesAdapter
+        eventCategoriesAdapter.notifyDataSetChanged()
     }
 }
