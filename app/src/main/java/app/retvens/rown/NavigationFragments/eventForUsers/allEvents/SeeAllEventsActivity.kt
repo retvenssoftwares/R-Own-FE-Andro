873a859cfb91c4bf3aa2b1d6189a0f2698cf972b
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-20 14:54:51 +0530

    EventFragment

---
+++ app/src/main/java/app/retvens/rown/NavigationFragments/eventForUsers/allEvents/SeeAllEventsActivity.kt
@@ -0,0 +1,12 @@
+package app.retvens.rown.NavigationFragments.eventForUsers.allEvents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.retvens.rown.R
+
+class SeeAllEventsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_see_all_events)
+    }
+}
