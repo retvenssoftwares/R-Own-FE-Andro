2fcc0b8d732d5328fde3588c0d52962c235a550e
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-26 11:21:39 +0530

    Blogs

--- app/src/main/java/app/retvens/rown/viewAll/viewAllBlogs/ViewAllCategoriesActivity.kt
+++ app/src/main/java/app/retvens/rown/viewAll/viewAllBlogs/ViewAllCategoriesActivity.kt
@@ -2,43 +2,61 @@ package app.retvens.rown.viewAll.viewAllBlogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import app.retvens.rown.ApiRequest.RetrofitBuilder
 import app.retvens.rown.NavigationFragments.exploreForUsers.blogs.ExploreBlogsAdapter
 import app.retvens.rown.NavigationFragments.exploreForUsers.blogs.ExploreBlogsData
 import app.retvens.rown.R
+import app.retvens.rown.databinding.ActivityViewAllCategoriesBinding
+import app.retvens.rown.databinding.ActivityViewAllCommmunitiesBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ViewAllCategoriesActivity : AppCompatActivity() {
+    lateinit var binding:ActivityViewAllCategoriesBinding
 
     lateinit var categoriesRecyclerView: RecyclerView
     lateinit var viewAllCategoriesAdapter: ViewAllCategoriesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_all_categories)
+        binding= ActivityViewAllCategoriesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.communityBackBtn.setOnClickListener { onBackPressed() }
 
         categoriesRecyclerView = findViewById(R.id.categoriesRecyclerView)
         categoriesRecyclerView.layoutManager = LinearLayoutManager(this)
         categoriesRecyclerView.setHasFixedSize(true)
 
-        val blogs = listOf<ViewAllCategoriesData>(
-            ViewAllCategoriesData("Title 1"),
-            ViewAllCategoriesData("Title 2"),
-            ViewAllCategoriesData("Title 3"),
-            ViewAllCategoriesData("Title 23"),
-            ViewAllCategoriesData("Title 1"),
-            ViewAllCategoriesData("Title 2"),
-            ViewAllCategoriesData("Title 3"),
-            ViewAllCategoriesData("Title 23"),
-            ViewAllCategoriesData("Title 1"),
-            ViewAllCategoriesData("Title 2"),
-            ViewAllCategoriesData("Title 3"),
-            ViewAllCategoriesData("Title 23"),
-        )
+        getAllCategory()
+    }
+    private fun getAllCategory() {
 
-        viewAllCategoriesAdapter = ViewAllCategoriesAdapter(blogs, this)
-        categoriesRecyclerView.adapter = viewAllCategoriesAdapter
-        viewAllCategoriesAdapter.notifyDataSetChanged()
+        val categories = RetrofitBuilder.viewAllApi.getBlogsCategory()
+        categories.enqueue(object : Callback<List<ViewAllCategoriesData>?> {
+            override fun onResponse(
+                call: Call<List<ViewAllCategoriesData>?>,
+                response: Response<List<ViewAllCategoriesData>?>
+            ) {
+
+                if (response.isSuccessful) {
+                    viewAllCategoriesAdapter =
+                        ViewAllCategoriesAdapter(response.body()!!, this@ViewAllCategoriesActivity)
+                    categoriesRecyclerView.adapter = viewAllCategoriesAdapter
+                    viewAllCategoriesAdapter.notifyDataSetChanged()
+                } else{
+                    Toast.makeText(applicationContext, "isNotSuccessful Blogs category: ${response.code()}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<ViewAllCategoriesData>?>, t: Throwable) {
+                Toast.makeText(applicationContext, "onFailure Blogs category: ${t.localizedMessage}", Toast.LENGTH_SHORT).show()
+            }
+        })
 
     }
 }
--- app/src/main/java/app/retvens/rown/viewAll/viewAllBlogs/ViewAllCategoriesData.kt
+++ app/src/main/java/app/retvens/rown/viewAll/viewAllBlogs/ViewAllCategoriesData.kt
@@ -1,3 +1,12 @@
 package app.retvens.rown.viewAll.viewAllBlogs
 
-data class ViewAllCategoriesData(val title : String)
+data class ViewAllCategoriesData(
+    val __v: Int,
+    val _id: String,
+    val category_id: String,
+    val category_name: String,
+    val Image: String,
+    val date_added: String,
+    val blog_count: Int,
+    val event_count: Int,
+)
