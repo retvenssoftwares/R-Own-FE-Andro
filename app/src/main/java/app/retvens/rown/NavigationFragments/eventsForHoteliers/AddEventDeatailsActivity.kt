7c8509a647b6ff6f628d39c00aa7f360dd578039
Arjun Gupta <arjungupta0817@gmail.com>
2023-05-27 16:53:32 +0530

    Add Events

--- app/src/main/java/app/retvens/rown/NavigationFragments/eventsForHoteliers/AddEventDeatailsActivity.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/eventsForHoteliers/AddEventDeatailsActivity.kt
@@ -3,21 +3,51 @@ package app.retvens.rown.NavigationFragments.eventsForHoteliers
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import app.retvens.rown.R
+import app.retvens.rown.databinding.ActivityAddEventBinding
+import app.retvens.rown.databinding.ActivityAddEventDeatailsBinding
 
 class AddEventDeatailsActivity : AppCompatActivity() {
-
-    lateinit var next : CardView
+    lateinit var binding : ActivityAddEventDeatailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_event_deatails)
+        binding = ActivityAddEventDeatailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        next = findViewById(R.id.next_Event)
-        next.setOnClickListener {
-            startActivity(Intent(this, EvenDetailsActivity::class.java))
+        val location = intent.getStringExtra("location")
+        val venue = intent.getStringExtra("venue")
+        val title = intent.getStringExtra("title")
+        val category_id = intent.getStringExtra("category_id")
+        val description = intent.getStringExtra("description")
+        val eventCategory = intent.getStringExtra("eventCategory")
+
+        binding.nextEvent.setOnClickListener {
+            if (binding.etEmailEvent.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter Title", Toast.LENGTH_SHORT).show()
+            } else if (binding.etPhoneVenue.text.toString()
+                    .isEmpty() || binding.etWebsite.text.toString()
+                    .isEmpty() || binding.etBooking.text.toString()
+                    .isEmpty() || binding.price.text.toString().isEmpty()
+            ) {
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, EvenDetailsActivity::class.java)
+                intent.putExtra("location", location)
+                intent.putExtra("venue", venue)
+                intent.putExtra("category_id", category_id)
+                intent.putExtra("title", title)
+                intent.putExtra("description", description)
+                intent.putExtra("eventCategory", eventCategory)
+                intent.putExtra("email", binding.etEmailEvent.text.toString())
+                intent.putExtra("phone", binding.etPhoneVenue.text.toString())
+                intent.putExtra("website", binding.etWebsite.text.toString())
+                intent.putExtra("booking", binding.etBooking.text.toString())
+                intent.putExtra("price", binding.price.text.toString())
+                startActivity(intent)
+            }
         }
-
     }
 }
--- app/src/main/java/app/retvens/rown/NavigationFragments/eventsForHoteliers/CreateEventActivity.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/eventsForHoteliers/CreateEventActivity.kt
@@ -3,19 +3,51 @@ package app.retvens.rown.NavigationFragments.eventsForHoteliers
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import app.retvens.rown.R
+import app.retvens.rown.bottomsheet.BottomSheetCountryStateCity
+import app.retvens.rown.bottomsheet.BottomSheetEventCategory
 import app.retvens.rown.databinding.ActivityCreateEventBinding
 
-class CreateEventActivity : AppCompatActivity() {
+class CreateEventActivity : AppCompatActivity(), BottomSheetEventCategory.OnBottomEcClickListener {
     lateinit var binding : ActivityCreateEventBinding
+
+    var category_id = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateEventBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.nextEventCreate.setOnClickListener {
-            startActivity(Intent(applicationContext, AddEventDeatailsActivity::class.java))
-        }
+        binding.profileBackBtn.setOnClickListener { onBackPressed() }
 
+        val location = intent.getStringExtra("location")
+        val venue = intent.getStringExtra("venue")
+
+        binding.eventCategory.setOnClickListener {
+            val bottomSheet = BottomSheetEventCategory()
+            val fragManager = supportFragmentManager
+            fragManager.let{bottomSheet.show(it, BottomSheetEventCategory.EC_TAG)}
+            bottomSheet.setOnECclickListener(this)
+        }
+        binding.nextEventCreate.setOnClickListener {
+            if (binding.eventNameEdit.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter Title", Toast.LENGTH_SHORT).show()
+            } else if (binding.eventDescriptionEdit.text.toString().isEmpty() || binding.eventCategory.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter Description", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(applicationContext, AddEventDeatailsActivity::class.java)
+                intent.putExtra("location", location)
+                intent.putExtra("venue", venue)
+                intent.putExtra("category_id", category_id)
+                intent.putExtra("title", binding.eventNameEdit.text.toString())
+                intent.putExtra("description", binding.eventDescriptionEdit.text.toString())
+                intent.putExtra("eventCategory", binding.eventCategory.text.toString())
+                startActivity(intent)
+            }
+        }
+    }
+    override fun bottomEcClick(eventC: String, NumericCodeFrBo: String) {
+        binding.eventCategory.setText(eventC)
+        category_id = NumericCodeFrBo
     }
 }
