bb98487e5a747a5a0e8f74b6af83f0f4a3f63b2b
ASUS <neerajraofficial@gmail.com>
2023-12-25 11:48:52 +0530

    Save job api integration

--- app/src/main/java/app/retvens/rown/NavigationFragments/job/savedJobs/SavedJobsData.kt
+++ app/src/main/java/app/retvens/rown/NavigationFragments/job/savedJobs/SavedJobsData.kt
@@ -1,5 +1,17 @@
 package app.retvens.rown.NavigationFragments.job.savedJobs
 
-data class SavedJobsData(
-    val jobs: List<Job>
+data class SavedJob(
+    val jobTitle: String,
+    val jobType: String,
+    val expectedCTC: String,
+    val jobLocation: String,
+    val companyImage: String? = null,  // Optional, as it's not present in all jobs
+    val companyName: String? = null     // Optional, as it's not present in all jobs
 )
+
+data class SavedJobsData(
+    val page: Int,
+    val pageSize: Int,
+    val jobs: List<SavedJob>
+)
+
